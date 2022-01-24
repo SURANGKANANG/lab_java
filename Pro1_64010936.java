567d662e2973323d320796ee06aec3a43db3c142
SURANGKANANG <88362423+SURANGKANANG@users.noreply.github.com>
2022-01-24 17:13:57 +0700

    Add files via upload

---
+++ Pro1_64010936.java
@@ -0,0 +1,7 @@
+public class Pro1_64010936 {
+    public static void main(String[] args) {
+        System.out.println(312032486 + (365 * 5 * 60 * 60 * 24) / 7 - (365 * 5 * 60 * 60 * 24) / 13
+                + (365 * 5 * 60 * 60 * 24) / 45);
+    }
+}
+    
